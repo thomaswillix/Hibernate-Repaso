8373d522696bc5248b37689b2c5ef42fc651de5f
Thomas <117021963+thomaswillix@users.noreply.github.com>
2024-05-12 14:14:30 +0200

    .
    Ejercicio  de Retenciones hecho.

--- Retenciones/src/hibernate/MainEj3.java
+++ Retenciones/src/hibernate/MainEj3.java
@@ -1,5 +1,6 @@
 package hibernate;
 
+import POJOS.Departamento;
 import POJOS.Empleado;
 import hibernate.Trabajadores;
 import java.io.EOFException;
@@ -54,18 +55,39 @@ public class MainEj3 {
         } catch (IOException ex) {
             Logger.getLogger(MainEj3.class.getName()).log(Level.SEVERE, null, ex);
         }
-        System.out.println("\n\n-------------HIBERNATE-------------");
         SessionFactory sf = HibernateUtil.sessionFactory();
         Session s = sf.openSession();
         Transaction tr = s.beginTransaction();
         Empleado e;
+        Departamento d;
+        System.out.println("\n\n------------- LISTA DE EMPLEADOS SIN ACTUALIZAR -------------");
         Query q = s.createQuery("FROM Empleado");
-        List<Empleado> empleados = q.list();
+        List<Empleado> list = q.list();
+        for (Empleado empleado : list) {
+            System.out.println(empleado.toString());
+        }
+        
         for (Trabajadores trabajador : trabajadores) {
             e  = (Empleado) s.get(Empleado.class, trabajador.getEmpNo());
             if (e==null) {
-                
+                System.err.println("El código introducido no pertenece a ningún empleado de la base de datos.");
+            } else{
+                d = (Departamento)s.get(Departamento.class, trabajador.getDept_no());
+                if (d!=null) {
+                    e.setDepartamento(d);
+                    e.setSalario(trabajador.getSalario());
+                    e.setNombre(trabajador.getNombre());
+                    s.saveOrUpdate(e);
+                }else System.err.println("El departamento que se indica no existe en la base de datos.");
             }
         }
+        tr.commit();
+        System.out.println("\n\n------------- LISTA DE EMPLEADOS ACTUALIZADOS -------------");
+        Query q2 = s.createQuery("FROM Empleado");
+        List<Empleado> list2 = q.list();
+        for (Empleado empleado : list2) {
+            System.out.println(empleado.toString());
+        }
+        System.exit(0);
     }
 }

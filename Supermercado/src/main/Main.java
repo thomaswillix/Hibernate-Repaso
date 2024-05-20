03acd6552e354541da2144bbc7272e66c46a9b87
Thomas <117021963+thomaswillix@users.noreply.github.com>
2024-05-20 11:42:34 +0200

    .
    Ej de Supermercado hecho

--- Supermercado/src/main/Main.java
+++ Supermercado/src/main/Main.java
@@ -29,14 +29,15 @@ public class Main {
         
         Ventas v;
         Productos p;
-        
+        int id;
         SessionFactory sf = HibernateUtil.sessionFactory();
         Session s = sf.openSession();
-        //Transaction t = s.beginTransaction();
+        Transaction t;
         
-        System.out.println("------- VENTAS -------");        
+        System.out.println("------- LISTA DE VENTAS -------");        
         Query q = s.createQuery("FROM Ventas");
         List<Ventas> lista = q.list();
+        id = lista.size() + 1;
         List<String> vendedores = new ArrayList<>();
         for (Ventas venta : lista) {
             System.out.println(venta.toString());
@@ -45,8 +46,7 @@ public class Main {
             }
         }
         
-        lista.removeAll(lista);
-        
+        List<Ventas> listaBin = new ArrayList<>();
         String codVend;
         int unidades;
         
@@ -58,7 +58,7 @@ public class Main {
                     p = (Productos) s.get(Productos.class, dis.readInt());
                     unidades = dis.readInt();
                     v = new Ventas(codVend, p, unidades);
-                    lista.add(v);
+                    listaBin.add(v);
                 }
             } catch (EOFException e) {
                 System.err.println("End of file");
@@ -68,7 +68,45 @@ public class Main {
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+        double ganancias;
+        System.out.println("\n\n--------- Lista de ventas en el archivo .dat ---------");
+        for (Ventas venta : listaBin) {
+            t = s.beginTransaction();
+            if (venta.getProductos() == null) {
+                System.err.println("ERROR: EL PRODUCTO ASOCIADO A LA VENTA NO EXISTE");
+            }else{ 
+                System.out.println(venta.getCodVend() + ", "  + venta.getProductos().getNombre()+", " + venta.getUnidades());
+                if(!vendedores.contains(venta.getCodVend())){
+                    System.out.println("ESE VENDEDOR NO FIGURA EN LA LISTA DE VENDEDORES, SE LE DARÁ DE ALTA");
+                    vendedores.add(venta.getCodVend());
+                    p = (Productos) s.get(Productos.class, venta.getProductos().getCodprod());
+                    ganancias = p.getPrecio() * venta.getUnidades();
+                    v = new Ventas(id, venta.getProductos(), venta.getCodVend(), venta.getUnidades(), ganancias);
+                    id++;
+                    s.save(v);
+                }else{
+                    for (Ventas ventaBD : lista) {
+                        if (ventaBD.getCodVend().equals(venta.getCodVend()) && 
+                                ventaBD.getProductos().getCodprod() == venta.getProductos().getCodprod()) {
+                     
+                            ventaBD.setUnidades(ventaBD.getUnidades() + venta.getUnidades());
+                            p = (Productos) s.get(Productos.class, venta.getProductos().getCodprod());
+                            ganancias = p.getPrecio() * venta.getUnidades();
+                            ventaBD.setGanancia(ganancias);
+                            s.saveOrUpdate(ventaBD);
+                        }
+                    }
+                }
+                t.commit();
+                
+            }
+        }
+        System.out.println("\n\n------------ LISTA DE VENTAS TRAS AÑADIR NUEVOS REGISTROS ------------");
+        q = s.createQuery("FROM Ventas");
+        lista = q.list();
+        for (Ventas venta : lista) {
+            System.out.println(venta.toString());
+        }
         
         s.close();
         sf.close();
